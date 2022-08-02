672b26113668785279b5fe3bcede093e8cc5214e
chrisqiqiang <2290142073@qq.com>
2022-08-02 23:27:11 +0800

    add global batches

--- scripts/tf_cnn_benchmarks/benchmark_cnn.py
+++ scripts/tf_cnn_benchmarks/benchmark_cnn.py
@@ -37,7 +37,7 @@ import numpy as np
 
 import six
 from six.moves import xrange  # pylint: disable=redefined-builtin
-import tensorflow.compat.v1 as global_variables
+import tensorflow.compat.v1 as tf
 
 # pylint: disable=g-direct-tensorflow-import
 import cnn_util
